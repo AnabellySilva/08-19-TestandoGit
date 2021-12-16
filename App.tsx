6f74ffc0fb80d63301c3e657a7c6d96b5170e829
AnabellySilva <90564362+AnabellySilva@users.noreply.github.com>
2021-12-16 14:05:49 -0400

    adaptado

--- App.tsx
+++ App.tsx
@@ -2,20 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import  Routes from './src/routes'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Routes/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
